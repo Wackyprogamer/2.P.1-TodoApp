702c86f60251f238879a9adfc6f90102792c7b71
Sergio Castillo <sergio.castillo5392@stu.mtec.edu>
2023-09-18 19:31:46 -0600

    added JavaScript Formula for Textbox entry to create a new list 09/18/2023 19:31:46

--- myScripts.js
+++ myScripts.js
@@ -1,5 +1,20 @@
-$('#myLists a').on('click', function (e) {
+$(document).on("keypress", "input", function(e) {
 
-e.preventDefault()
-$(this).tab('show')
-})
+    if (e.which == 13) {
+
+        let text = document.getElementById('listName').value;
+
+        let a = document.createElement('a');
+
+        a.type = 'button';
+
+        a.className = 'list-group-item list-group-item-action';
+
+        a.textContent = text;
+
+        let parentDiv = document.querySelector('#myLists')
+
+        parentDiv.appendChild(a);
+
+    }
+});
